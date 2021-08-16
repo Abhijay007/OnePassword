33880abda3b31bffd11f19958f7cf60258d149a9
Abhijay007j <Abhijay007j@gmail.com>
2021-08-16 23:44:56 +0530

    contact form functionality update

--- process.php
+++ process.php
@@ -1,14 +1,15 @@
 <?php
-$name = $_REQUEST['name'];
-$email = $_REQUEST['Email'];
-$message = $_REQUEST['Message'];
+$name = $_REQUEST['form-name'];
+$email = $_REQUEST['form-email'];
+$subject = $_REQUEST['form-subject'];
+$message = $_REQUEST['form-message'];
 
 if (empty($name) || empty($email) || empty($message)) {
     echo "<script type='text/javacript> alert('please Fill all the Fields');
     </script>";
 } else {
-    mail("aventures.tech@gmail.com", "new form submission", $message, "From:$name <$email>");
-    
+    mail("aventures.tech@gmail.com", $subject, $message, "From:$name <$email>");
+
     echo "<script type='text/javascript>
     alert('Your message is sent successfully');
     window.history.log(-1);
